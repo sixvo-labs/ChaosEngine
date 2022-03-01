873fc4c143ec19adcf90b26c4eee7471e54f4935
sixvo-labs <dev@sixvolabs.com>
2022-02-28 21:57:44 -0600

    fixed server.py, restructered directory tree

--- Tests/00/server.py
+++ Tests/00/server.py
@@ -1,3 +1,15 @@
+
+#sudo apt-get install python-requests
+#sudo pip install flask
+#sudo pip install sensehat
+
+from sense_hat import SenseHat
+import time
+from datetime import datetime
+import requests
+sense = SenseHat()
+degree_symbol = u"\N{DEGREE SIGN}"
+
 from sense_hat import SenseHat
 import time
 import requests # sudo apt-get install python-requests
@@ -15,21 +27,21 @@ def hello():
 def temperature():
     t = sense.get_temperature() * (9/5) + 32 # convert to degrees F
     t = str(round(t, 6)) # convert to string
-    temperature_now = 'Temperature ' + t + degree_symbol + ' recorded at ' + ti$
+    temperature_now = 'Temperature ' + t + degree_symbol + ' recorded at ' + time.strftime("%H:%M:%S:%f", time.localtime())
     return temperature_now
 
 @app.route("/humidity")
 def humidity():
     h = sense.get_humidity()
     h = str(round(h, 6))
-    humidity_now = 'Humidity: ' + h + '% recorded at' + time.strftime("%H:%M:%S$
+    humidity_now = 'Humidity: ' + h + '% recorded at' + time.strftime("%H:%M:%S", time.localtime())
     return humidity_now
 
 @app.route("/pressure")
 def pressure():
     p = sense.get_pressure() / 33.864 # convert to inHg (inches of mercury)
     p = str(round(p, 6))
-    pressure_now = 'Pressure ' + p + 'inHg recorded at ' + time.strftime("%H:%M$
+    pressure_now = 'Pressure ' + p + 'inHg recorded at ' + time.strftime("%H:%M:%S:%f", time.localtime())
     return pressure_now
 
 @app.route("/snapshot")
@@ -57,11 +69,11 @@ def snapshot():
     z = str(round(z, 5))
 
     snap = [t,h,p,c,r,w,x,y,z]
-    snapshot = "{" + "snapshot: " + time.strftime("%H:%M:%S", time.localtime())$
-               ", data: " str(snap) + "}"
+    snapshot = "{" + "snapshot: " + str(datetime.now()) + ", " + str(snap) + "}"
+    #snapshot = "{" + "snapshot: " + time.strftime("%H:%M:%S:%f", time.localtime()) + ", " + str(snap) + "}"
     return snapshot
 
-    #print("{0},{1},{2},{3},{4},{5},{6},{7},{8} ".format(t, h, p, pitch, roll, $
+    #print("{0},{1},{2},{3},{4},{5},{6},{7},{8} ".format(t, h, p, pitch, roll, yaw, x, y, z)
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0", port=80, debug=True)
---
+++ analysis/readME.md
@@ -0,0 +1,6 @@
+#Analysis Notes:
+
+###todo 
+- evaluate data fully and properly
+
+note: Evidence is better labeed as insights
